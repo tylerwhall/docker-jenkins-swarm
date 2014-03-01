14647d1f41e7ea3de01eb086eaff0df1761b99b9
Tyler Hall <tylerwhall@gmail.com>
2014-03-01 18:11:49 -0500

    run_etcd: Fix certificate binding
    Need to actually make the directory available to the container...

--- run_etcd.sh
+++ run_etcd.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 PUBLIC_IP=${PUBLIC_IP:-127.0.0.1}
 ETCD_AUTHDIR=${ETCD_AUTHDIR:-./ca}
 
@@ -8,14 +10,15 @@ CERT="$ETCD_AUTHDIR/client.crt"
 KEY="$ETCD_AUTHDIR/client.key"
 
 if [ -f "$CERT" -a -f "$KEY" ]; then
-    ETCD_AUTH="-cert-file=$CERT -key-file=$KEY"
+    DOCKER_OPTS="-v $ETCD_AUTHDIR:/auth"
+    ETCD_AUTH="-cert-file=/auth/client.crt -key-file=/auth/client.key"
     echo "Using server SSL"
     if [ -f "$CACERT" ]; then
-        ETCD_AUTH="$ETCD_AUTH -ca-file=$CACERT"
+        ETCD_AUTH="$ETCD_AUTH -ca-file=/auth/ca.crt"
         echo "Using client SSL"
     fi
 else
     echo "Authentication files not found. Proceeding without."
 fi
 
-exec docker run -d -p 8001:8001 -p 5001:5001 coreos/etcd -peer-addr ${PUBLIC_IP}:8001 -addr ${PUBLIC_IP}:5001 -name `hostname` $ETCD_AUTH
+exec docker run -d -p 8001:8001 -p 5001:5001 $DOCKER_OPTS coreos/etcd -peer-addr ${PUBLIC_IP}:8001 -addr ${PUBLIC_IP}:5001 -name `hostname` $ETCD_AUTH
