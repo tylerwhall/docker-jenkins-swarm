c3097d700050b58f3f11df308d6e7e5ea66d1b6c
Tyler Hall <tylerwhall@gmail.com>
2014-03-01 17:48:06 -0500

    Initial jenkins swarm client

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM fedora:20
+
+RUN yum -y install java-1.8.0-openjdk curl jq redhat-lsb-core
+
+RUN adduser -d /var/lib/jenkins jenkins
+
+# Add the swarm client
+RUN curl http://maven.jenkins-ci.org/content/repositories/releases/org/jenkins-ci/plugins/swarm-client/1.15/swarm-client-1.15-jar-with-dependencies.jar > /var/lib/jenkins/swarm-client.jar
+ADD jenkins_start.sh /var/lib/jenkins/
+
+ENTRYPOINT ["/bin/su", "jenkins", "-c", "/var/lib/jenkins/jenkins_start.sh"]
---
+++ jenkins_start.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+if [ -z "$ETCD_URL" ]; then
+    echo 'Must specify $ETCD_URL in environment'
+    exit 1
+fi
+
+ETCD_AUTHDIR=${ETC_AUTHDIR:-./ca2}
+CACERT="$ETCD_AUTHDIR/ca.crt"
+CERT="$ETCD_AUTHDIR/client.crt"
+KEY="$ETCD_AUTHDIR/client.key"
+
+if [ -f "$CACERT" -a -f "$CERT" -a -f "$KEY" ]; then
+    CURL_AUTH="--cacert $CACERT --cert $CERT --key $KEY"
+else
+    echo "Authentication files not found. Proceeding without."
+fi
+
+function get_etcd_value () {
+    json=`curl $CURL_AUTH -s -L "$ETCD_URL/v2/keys/$2"`
+    if [ $? -ne 0 ]; then echo "curl failed to connect to etcd at $ETCD_URL"; exit 1; fi
+    val=`echo $json | jq -r .node.value`
+    [ "$val" == "null" ] && val=""
+    eval $1="$val"
+}
+
+get_etcd_value JENKINS_URL jenkins/url
+if [ -z "$JENKINS_URL" ]; then
+    echo "Unable to retrieve jenkins url from etcd."
+fi
+
+DISTRO=`lsb_release -irs | tr -d ' '`
+
+get_etcd_value JENKINS_USERNAME jenkins/username
+get_etcd_value JENKINS_PASSWORD jenkins/password
+
+if [ -n "$JENKINS_USERNAME" -a -n "$JENKINS_PASSWORD" ]; then
+    JENKINS_USERPASS="-username $JENKINS_USERNAME -password $JENKINS_PASSWORD"
+else
+    echo "User/pass not found. Using no Jenkins authentication"
+fi
+
+SWARM_LABELS="$SWARM_LABELS $DISTRO"
+
+cmd="java -jar $HOME/swarm-client.jar -master "$JENKINS_URL" $JENKINS_USERPASS -labels '$SWARM_LABELS' $SWARM_OPTIONS"
+echo $cmd
+eval $cmd
