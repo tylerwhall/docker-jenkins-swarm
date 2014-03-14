17dd4a0c610507e08d292931f0ea65bc6aecce99
Tyler Hall <tylerwhall@gmail.com>
2014-03-13 20:57:29 -0400

    populate_etcd: Fix typo

--- populate_etcd.sh
+++ populate_etcd.sh
@@ -14,6 +14,6 @@ read -p "Jenkins URL: " url
 read -p "Jenkins client username: " username
 read -p "Jenkins client password: " password
 set -ex
-curl $CURL_OPTS $ETCD_URL/v2/keys/jenkins/url -XPUT -d value=$ur
+curl $CURL_OPTS $ETCD_URL/v2/keys/jenkins/url -XPUT -d value=$url
 curl $CURL_OPTS $ETCD_URL/v2/keys/jenkins/username -XPUT -d value=$username
 curl $CURL_OPTS $ETCD_URL/v2/keys/jenkins/password -XPUT -d value=$password
